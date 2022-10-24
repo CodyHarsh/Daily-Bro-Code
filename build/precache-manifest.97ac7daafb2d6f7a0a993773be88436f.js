045db0a91783a773ba45ff93fd4388dee6d211e4
Harsh <71979122+CodyHarsh@users.noreply.github.com>
2022-10-24 15:52:36 +0530

    Add files via upload

---
+++ build/precache-manifest.97ac7daafb2d6f7a0a993773be88436f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f5d4552307588da6917eda372d2ddf1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad44b178a130deceebbe",
+    "url": "/static/css/2.a07d8c99.chunk.css"
+  },
+  {
+    "revision": "ee2d1a4da02d398855fd",
+    "url": "/static/css/main.1d765dca.chunk.css"
+  },
+  {
+    "revision": "ad44b178a130deceebbe",
+    "url": "/static/js/2.b1132adc.chunk.js"
+  },
+  {
+    "revision": "5467e851392549ae3564d75adbc67604",
+    "url": "/static/js/2.b1132adc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee2d1a4da02d398855fd",
+    "url": "/static/js/main.b40ea0a4.chunk.js"
+  },
+  {
+    "revision": "3b33fffe9e6ff119bba8",
+    "url": "/static/js/runtime-main.4fb6a82e.js"
+  }
+]);
